9069e81c7fdcd8f16336eba2ea55ecedd9715b83
Sayantan <sayantanhalder43@gmail.com>
2025-09-16 19:07:14 +0530

    added color to header2

--- Day 2/first.js
+++ Day 2/first.js
@@ -5,3 +5,9 @@
 const obj = document.getElementById("first");
 obj.innerHTML = "Header2";
 console.log(obj.__proto__); // HTMLElement prototype
+
+// Get elements by class name → returns an HTMLCollection (array-like object)
+const obj2 = document.getElementsByClassName("header2");
+obj2[0].innerHTML = "HEader2"; //accesing obj2[0] means header2
+
+obj2[0].style.color = "Violet";
