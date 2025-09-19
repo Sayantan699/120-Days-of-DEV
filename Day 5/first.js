2e93a6410f9221c1919de404b6940e570420c4ca
Sayantan <sayantanhalder43@gmail.com>
2025-09-20 00:46:14 +0530

    added event delegation

--- Day 5/first.js
+++ Day 5/first.js
@@ -1,13 +1,26 @@
-const buttons = document.querySelectorAll("button");
-function changecolor() {
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      document.body.style.backgroundColor = button.id;
-    });
-  });
-}
-changecolor();
+// const buttons = document.querySelectorAll("button");
+// function changecolor() {
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//       document.body.style.backgroundColor = button.id;
+//     });
+//   });
+// }
+// changecolor();
 
-setInterval(() => {
-  document.body.style.backgroundColor = "black";
-}, 3000);
+//Using event delegation
+const root = document.getElementById("root");
+root.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    //event.target refers to the button i m clcking
+    //event.target.tagName always returns the tag name in UPPERCASE (e.g., "BUTTON", "DIV", "SPAN").
+    document.body.style.backgroundColor = event.target.id;
+  }
+  //   if (event.target.tagName === "button") {
+  //     document.body.style.backgroundColor = event.target.id;
+  //   }
+
+  setTimeout(() => {
+    document.body.style.backgroundColor = "black";
+  }, 3000);
+});
