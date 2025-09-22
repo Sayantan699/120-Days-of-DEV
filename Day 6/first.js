c1fd8aabdbe0f1951b42616aee939dd561ac356a
Sayantan <sayantanhalder43@gmail.com>
2025-09-23 00:21:46 +0530

    Modified

--- Day 6/first.js
+++ Day 6/first.js
@@ -2,3 +2,11 @@ const form = document.querySelector("form");
 form.addEventListener("click", (event) => {
   console.log(event.target.value);
 });
+
+//to submit the form
+
+form.addEventListener("submit", (event) => {
+  //page refresh on usbmit
+  //   console.log(event.target.value);
+  console.log("Form Submited!");
+});
